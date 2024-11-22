36cce9ce7d3f4e78717910a1f0ff00cf6aec3ab9
unknown <satyam02367@gmail.com>
2024-11-23 00:15:05 +0530

    Navabar setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Navbar from "@/componenets/ui/navbar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const inter = Inter({subnets: ["latin"]})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,11 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="relative w-full items-center justify-center">
+          <Navbar />
+        </div>
       </body>
     </html>
   );
